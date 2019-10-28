b5c7a8dec79d29f0d32c8bad39b925675c4aba13
Michael Gorzelsky <56135204+mgorzelsky@users.noreply.github.com>
2019-10-27 23:12:48 -0700

    Add files via upload

---
+++ CSharp/Reverse-String/ReverseString.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public static class ReverseString
+{
+    public static string Reverse(string input)
+    {
+        char[] aString = new char[input.Length];
+        int count = 0;
+        foreach (char letter in input)
+        {
+            aString[count] = letter;
+            count++;
+        }
+
+        Array.Reverse(aString);
+        string reversedInput = new string(aString);
+        return reversedInput;
+    }
+}
