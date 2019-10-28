5891da3a6e2bc2fd73fe07c649bb9ab0c06e16dc
Michael Gorzelsky <56135204+mgorzelsky@users.noreply.github.com>
2019-10-27 21:23:41 -0700

    Add files via upload

---
+++ CSharp/Hamming/Hamming.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+
+public static class Hamming
+{
+    public static int Distance(string firstStrand, string secondStrand)
+    {
+        firstStrand.ToLower();
+        secondStrand.ToLower();
+        char[] firstStrandArray = firstStrand.ToCharArray();
+        char[] secondStrandArray = secondStrand.ToCharArray();
+        int hamming = 0;
+
+        if (firstStrandArray.Length != secondStrandArray.Length)
+            throw new ArgumentException("The strands must be the same length.");
+
+        for (int i = 0; i < firstStrandArray.Length; i++)
+        {
+            if (firstStrandArray[i] != secondStrandArray[i])
+                hamming++;
+        }
+        return hamming;
+    }
+}
