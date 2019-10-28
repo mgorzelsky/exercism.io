d4d9fd4808df88323367b7ee85927f2ac248289b
Michael Gorzelsky <56135204+mgorzelsky@users.noreply.github.com>
2019-10-27 23:38:12 -0700

    Add files via upload

---
+++ CSharp/Sum-Of-Multiples/SumOfMultiples.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class SumOfMultiples
+{
+    public static int Sum(int[] multiples, int max)
+    {
+        int sum = 0;
+        for (int i = max - 1; i >= 0; i--)
+        {
+            for (int j = 0; j < multiples.Length; j++)
+                if (multiples[j] > 0 && i % multiples[j] == 0)
+                {
+                    sum += i;
+                    break;
+                }
+        }
+        return sum;
+    }
+}
