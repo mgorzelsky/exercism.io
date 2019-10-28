a98b807385b54a49ac9cd019557af0e33cb20500
Michael Gorzelsky <56135204+mgorzelsky@users.noreply.github.com>
2019-10-28 10:13:46 -0700

    Add files via upload

---
+++ CSharp/Strain/Strain.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+
+public static class Strain
+{
+    public static IEnumerable<T> Keep<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
+    {
+        List<T> collectionKeep = new List<T>();
+        foreach (var item in collection)
+        {
+            if (predicate(item))
+                collectionKeep.Add(item);
+        }
+        return collectionKeep;
+    }
+
+    public static IEnumerable<T> Discard<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
+    {
+        List<T> collectionDiscard = new List<T>();
+        foreach (var item in collection)
+        {
+            if (!predicate(item))
+                collectionDiscard.Add(item);
+        }
+        return collectionDiscard;
+    }
+}
