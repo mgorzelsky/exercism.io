e6b9df829f0a282aa713e0ba4c5160cd67351d88
Michael Gorzelsky <56135204+mgorzelsky@users.noreply.github.com>
2019-10-28 19:48:41 -0700

    Add files via upload

---
+++ CSharp/Armstrong-Numbers/ArmstrongNumbers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class ArmstrongNumbers
+{
+    public static bool IsArmstrongNumber(int number)
+    {
+        string numberAsString = number.ToString();
+        List<int> numberAsList = new List<int>();
+        foreach (char digit in numberAsString)
+        {
+            numberAsList.Add(Convert.ToInt32(digit.ToString()));
+        }
+
+        int total = 0;
+        foreach (int digit in numberAsList)
+            total += Convert.ToInt32(Math.Pow(digit, numberAsList.Count));
+        if (total == number)
+            return true;
+        else
+            return false;
+    }
+}
