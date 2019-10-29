70cf0e30ed8586fa3532924997facd888c81d92c
Michael Gorzelsky <56135204+mgorzelsky@users.noreply.github.com>
2019-10-29 11:20:17 -0700

    Add files via upload

---
+++ CSharp/Pangram/Pangram.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input)
+    {
+        Dictionary<char, int> alphabet = new Dictionary<char, int>();
+        for (char i = 'a'; i <= 'z'; i++)
+            alphabet.Add(i, 0);
+
+        foreach (char letter in input.ToLower())
+        {
+            alphabet[letter] = 1;
+        }
+
+        foreach (KeyValuePair<char, int> entry in alphabet)
+        {
+            if (entry.Value == 0)
+                return false;
+        }
+        return true;
+
+    }
+}
