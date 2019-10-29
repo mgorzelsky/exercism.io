b0ec7ba751f3beb2ae0c33485879901b3dd6491a
Michael Gorzelsky <56135204+mgorzelsky@users.noreply.github.com>
2019-10-28 20:30:06 -0700

    Add files via upload

---
+++ CSharp/rna-transcription/RnaTranscription.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Text;
+
+    //G -> C
+    //C -> G
+    //T -> A
+    //A -> U
+
+public static class RnaTranscription
+{
+    public static string ToRna(string nucleotide)
+    {
+        StringBuilder rna = new StringBuilder();
+        foreach (char letter in nucleotide)
+        {
+            switch (letter)
+            {
+                case ('G'):
+                    rna.Append('C');
+                    break;
+                case ('C'):
+                    rna.Append('G');
+                    break;
+                case ('T'):
+                    rna.Append('A');
+                    break;
+                case ('A'):
+                    rna.Append('U');
+                    break;
+            }
+        }
+        return rna.ToString();
+    }
+}
