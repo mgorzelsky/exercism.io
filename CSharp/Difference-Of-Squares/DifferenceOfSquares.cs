e32252580c00dd8d4ebf0f60ac207267ea2a6ea1
Michael Gorzelsky <56135204+mgorzelsky@users.noreply.github.com>
2019-10-29 09:16:09 -0700

    Add files via upload

---
+++ CSharp/Difference-Of-Squares/DifferenceOfSquares.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class DifferenceOfSquares
+{
+    public static int CalculateSquareOfSum(int max)
+    {
+        int sum = 0;
+        int[] naturalNumbers = new int[max];
+        for (int i = 1; i <= max; i++)
+        {
+            naturalNumbers[i - 1] = i;
+        }
+        foreach (int number in naturalNumbers)
+            sum += number;
+
+        return Convert.ToInt32(Math.Pow(sum, 2));
+    }
+
+    public static int CalculateSumOfSquares(int max)
+    {
+        int sum = 0;
+        int[] naturalNumbers = new int[max];
+        for (int i = 1; i <= max; i++)
+        {
+            naturalNumbers[i - 1] = i;
+        }
+        foreach (int number in naturalNumbers)
+            sum += Convert.ToInt32(Math.Pow(number, 2));
+
+        return sum;
+    }
+
+    public static int CalculateDifferenceOfSquares(int max)
+    {
+        return CalculateSquareOfSum(max) - CalculateSumOfSquares(max);
+    }
+}
