dfde3a66c1ab8984fef645a29eed213bf6209e62
mgorzelsky <m.d.gorzelsky@gmail.com>
2019-12-05 13:11:32 -0800

    completed space-age on javascript track.

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -1,8 +1,15 @@
-//
-// This is only a SKELETON file for the 'Space Age' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export const age = () => {
-  throw new Error("Remove this statement and implement this function");
+export const age = (planet, seconds) => {
+  return +(ageOnPlanet.get(planet)(seconds)).toFixed(2);
 };
+
+const earthYearInSeconds = 365.25 * 24 * 3600;
+const ageOnPlanet = new Map([
+  ['mercury', (seconds) => ageOnPlanet.get('earth')(seconds) / 0.2408467],
+  ['venus', (seconds) => ageOnPlanet.get('earth')(seconds) / 0.61519726],
+  ['earth', (seconds) => seconds / earthYearInSeconds],
+  ['mars', (seconds) => ageOnPlanet.get('earth')(seconds) / 1.88081587],
+  ['jupiter', (seconds) => ageOnPlanet.get('earth')(seconds) / 11.862615],
+  ['saturn', (seconds) => ageOnPlanet.get('earth')(seconds) / 29.447498],
+  ['uranus', (seconds) => ageOnPlanet.get('earth')(seconds) / 84.016846],
+  ['neptune', (seconds) => ageOnPlanet.get('earth')(seconds) / 164.79132]
+]);
